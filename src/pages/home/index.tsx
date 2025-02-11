6bd00d5330cb0f21c033fe883bc42e9190c6eceb
Mariana Rossi <wmarianarossi@gmail.com>
2025-02-10 22:28:51 -0300

    removing unused function

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -46,15 +46,3 @@ const HomePage = () => {
   };
   
   export default HomePage;
-
-// export function HomePage() {
-//     return (
-//         <>
-//             <NavBar />
-//             <main></main> 
-//             {/* <h1 className="h3 mb-3 fw-normal">HOME PAGE</h1> */}
-//             <CarouselComponent/>
-//             <FooterComponent />
-//         </>
-//     );
-// }
